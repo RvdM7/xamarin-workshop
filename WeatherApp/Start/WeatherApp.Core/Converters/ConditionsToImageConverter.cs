7fec73aa79133b2f896ceb86511a09b37ca22c54
Maurice Markvoort <m.markvoort@xablu.com>
2018-05-04 16:55:59 +0200

    Converter added in starter project

---
+++ WeatherApp/Start/WeatherApp.Core/Converters/ConditionsToImageConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using WeatherApp.Core.Models;
+using Xamarin.Forms;
+
+namespace WeatherApp.Core.Converters
+{
+    public class ConditionsToImageConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ImageSource.FromFile(((Conditions)value).ToString().ToLower());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
