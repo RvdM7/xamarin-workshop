7df9ff276b3f10204e2fb15169c4c2cb80df308c
Maurits van Beusekom <maurits@vnbskm.nl>
2018-05-04 12:31:21 +0200

    Added TodoApp project

---
+++ TodoApp/Completed/TodoApp/Models/TodoItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace TodoApp.Models
+{
+    public class TodoItem
+    {
+        public bool IsDone { get; set; }
+        public string Title { get; set; }
+
+        public override string ToString()
+        {
+            return Title;
+        }
+    }
+}
+
---
+++ TodoApp/Completed/TodoApp/Utilities/NotifyPropertyChanged.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TodoApp.Utilities
+{
+    public class NotifyPropertyChanged : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        protected void SetProperty<T>(
+            ref T backingStore,
+            T value,
+            [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return;
+
+            backingStore = value;
+
+            OnPropertyChanged(propertyName);
+
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
